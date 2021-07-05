59f9b7676ac59b22f82216f890de10d132ceb057
MUSHIN26 <mushin26@gmail.com>
2021-07-05 18:35:25 +0000

    mixed lists

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+my_list = [ "192.168.0.5", 5060, "UP" ]
+# À noter que le 5060 est un integer et non un strin  
+print("The first item in the list (IP): " + my_list[0] )
+print("The second item in the list (port): " + str(my_list[1]) )
+print("The last item in the list (state): " + my_list[2] )
---
+++ mix-list/mixlist02.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+my_ip = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+print("The IPs are: " + my_ip[3] + " and " + my_ip[4])
+print("The Ips are:", my_ip[3],"and", my_ip[4])
+print(f"The Ips are: {my_ip[3]} and {my_ip[4]}")
+
